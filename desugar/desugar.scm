de1281765a9820b3aa8cdbd77ab3a5e36519010f
tca <theconartist@gmail.com>
2014-08-24 23:25:24 -0500

    * desugar lambda bodies with begin wrapper * changed begin desugar to not use named-let * remove letrec, named-let desugarers

--- desugar/desugar.scm
+++ desugar/desugar.scm
@@ -1,19 +1,10 @@
 ;; http://matt.might.net/articles/desugaring-scheme/
 
-;; need to lift up "define" vars to the top of lexical scope
-;; but they aren't actually set! until their original position
-;; (define (a) (b))
-;; (a) => error
-;; (define (b) 1)
-;; (a) => 1
-
-
 ;; checklist
 ;; ----
 ;; define
 ;; let - done
 ;; let* - done
-;; letrec
 ;; if - done
 ;; cond - done
 ;; and - done
@@ -58,19 +49,6 @@
        (list? (cadr exp))
        (equal? 'let* (car exp))))
 
-;; (let a ((b c)) (a c))
-(define (term-name-let? exp)
-  (and (list? exp)
-       (>= (length exp) 4)
-       (symbol? (cadr exp))
-       (list? (caddr exp))
-       (equal? 'let (car exp))))
-
-(define (term-letrec? exp)
-  (and (list? exp)
-       (>= (length exp) 3)
-       (list? (cadr exp))
-       (equal? 'letrec (car exp))))
 
 ;; (define a b)
 (define (term-define? exp)
@@ -131,100 +109,74 @@
    ((term-set!? exp) `(set! ,(cadr exp) ,(desugar (caddr exp))))
 
    ((term-begin? exp)
-    (let loop ((terms (map desugar (cdr exp))))
-      (if (null? terms)
-	  #f
-	  (if (null? (cdr terms))
-	      (desugar (car terms))
-	      `((lambda () ,(loop (cdr terms))) ,(car terms))))))
-   
-   
-   ;; (let loop ((foo bar)) .... (loop (baz foo)))
-   ((term-name-let? exp)
-    (let* ((name (cadr exp))
-	   (bindings (caddr exp))
+    (cond ((null? (cdr exp)) (error "empty begin form"))
+          ((null? (cddr exp)) (desugar (cadr exp)))
+          (else `((lambda () ,(desugar (cons 'begin (cddr exp)))) (desugar (cadr exp))))))
+
+   ((term-lambda? exp)
+    (let ((params (cadr exp))
+          (body (cddr exp)))
+      `(lambda ,params ,(desugar (cons 'begin body)))))
+
+   ((term-let? exp)
+    (let* ((bindings (cadr exp))
 	   (params (map car bindings))
 	   (values (map cadr bindings))
-	   (body (cdddr exp)))
-      (desugar `(letrec ((,name `(lambda ,params . body)))
-		  (,name . ,values)))))
-
-    ((term-let? exp)
-     (let* ((bindings (cadr exp))
-            (params (map car bindings))
-            (values (map cadr bindings))
-            (body (cddr exp)))
-       `((lambda ,params . ,(map desugar body)) . ,(map desugar values))))
-    
-    ((term-let*? exp)
-     (let* ((bindings (cadr exp))
-            (body (cddr exp)))
-       (desugar
-        (append `(let (,(car bindings)))
-                (if (null? (cdr bindings))
-                    body
-                    `((let* ,(cdr bindings) . ,body)))))))
-    
-    ((term-letrec? exp)
-     (let ((bindings (cadr exp))
 	   (body (cddr exp)))
-       (desugar `(let (map (lambda (p) (list p '(void))) (map car bindings))
-		   ,@(map (lambda (b) `(set! ,(car b) ,(cadr b))) bindings)
-                   ,@body))))
+      (desugar `((lambda ,params . ,body) . ,values))))
+   
+   ((term-let*? exp)
+    (let* ((bindings (cadr exp))
+	   (body (cddr exp)))
+      (desugar
+       (append `(let (,(car bindings)))
+               (if (null? (cdr bindings))
+                   body
+                   `((let* ,(cdr bindings) . ,body)))))))
 
-    ;; (define foo 1)
-    ;; ((term-define? exp)
-    ;;  (let ((name (cadr exp))
-    ;;        (value (caddr exp)))
-    ;;    (desugar `(let ((,name '()))
-    ;;                (set! ,name ,(desugar value))))))
-    
-    ;; (define (foo a) a ...)
-    ((term-define-procedure? exp)
-     (let ((name (caadr exp))
-           (params (cdadr exp))
-           (body (cddr exp)))
-       (desugar `(define ,name (lambda ,params . ,body)))))
-    
-    ;; if
-    ((term-if? exp)
-     (let ((b (cadr exp))
-           (then (caddr exp))
-           (else (cadddr exp)))
-       `(boolean ,(desugar b)
-                 (lambda () ,(desugar then))
-                 (lambda () ,(desugar else)))))
-    
-    ;; cond
-    ((term-cond? exp)
-     (cond
-       ;; (cond)
-       ((equal? '(cond) exp) '(void))
-       ;; (cond (else b)
-       ((and (equal? 'else (caadr exp))
-             (= 2 (length exp)))
-        (desugar (cadadr exp)))
-       ;; (cond (a b))
-       ((= 2 (length exp))
-            `(if ,(desugar (caadr exp))
-                ,(desugar (cadadr exp))
-                (void)))
-       ;; (coond (a b) rest ...)
-       ((> (length exp) 2)
-        `(if ,(desugar (caadr exp))
-             ,(desugar (cadadr exp))
-             ,(desugar `(cond . ,(cddr exp)))))))
-    
-    ((term-and? exp)
-     (let ((v1 (cadr exp))
-           (v2 (caddr exp)))
-       (desugar `(if ,v1 ,v2 #f))))
-    
-    ((term-or? exp)
-     (let ((v1 (cadr exp))
-           (v2 (caddr exp)))
-       (desugar `(if ,v1 #t ,v2))))
-    
+   ((term-define-procedure? exp)
+    (let ((name (caadr exp))
+	  (params (cdadr exp))
+	  (body (cddr exp)))
+      `(define ,name ,(desugar `(lambda ,params . ,body)))))
+   
+   ((term-if? exp)
+    (let ((b (cadr exp))
+	  (then (caddr exp))
+	  (else (cadddr exp)))
+      `(boolean ,(desugar b)
+		(lambda () ,(desugar then))
+		(lambda () ,(desugar else)))))
+   
+   ((term-cond? exp)
+    (cond
+     ;; (cond)
+     ((equal? '(cond) exp) ''())
+     ;; (cond (else b)
+     ((and (equal? 'else (caadr exp))
+	   (= 2 (length exp)))
+      (desugar (cadadr exp)))
+     ;; (cond (a b))
+     ((= 2 (length exp))
+      `(if ,(desugar (caadr exp))
+	   ,(desugar (cadadr exp))
+	   ''()))
+     ;; (coond (a b) rest ...)
+     ((> (length exp) 2)
+      `(if ,(desugar (caadr exp))
+	   ,(desugar (cadadr exp))
+	   ,(desugar `(cond . ,(cddr exp)))))))
+   
+   ((term-and? exp)
+    (let ((v1 (cadr exp))
+	  (v2 (caddr exp)))
+      (desugar `(if ,v1 ,v2 #f))))
+   
+   ((term-or? exp)
+    (let ((v1 (cadr exp))
+	  (v2 (caddr exp)))
+      (desugar `(if ,v1 #t ,v2))))
+   
     ((term-quote? exp)
      (quote-desugar (cadr exp)))
     ((term-quasiquote? exp)
