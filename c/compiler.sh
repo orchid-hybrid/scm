ac4af0ede5f0bc8a0e9dcd8fa52d2006a40595d7
tca <theconartist@gmail.com>
2014-08-25 01:05:47 -0500

    add prelude.scm

--- c/compiler.sh
+++ c/compiler.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-csi ../utility/* ../parser/parser.scm ../desugar/desugar.scm ../closure-conversion/closure-conversion.scm ../hoist/hoist.scm ../c/c.scm -e "(compile-c-definitions (map desugar (scm-parse-file \"$1\")))" > $1.c
+csi ../utility/* ../parser/parser.scm ../desugar/desugar.scm ../closure-conversion/closure-conversion.scm ../hoist/hoist.scm ../c/c.scm -e "(compile-c-definitions (map desugar (append (scm-parse-file \"../prelude/prelude.scm\") (scm-parse-file \"$1\"))))" > $1.c
